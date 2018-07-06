6cac413a1a843b2734b6f5c26d2b8de11ad6e378
ruchi <ruchi@thoughtclan.com>
2018-07-06 12:18:23 +0530

    Add Properties outside

--- entities/employee.py
+++ entities/employee.py
@@ -7,8 +7,8 @@ from properties import *
 db=_mysql.connect(db_host,db_user,db_password,db_name)
 
 class Employee(Model):
-	employee_id = PrimaryKeyField(primary_key=True)
-	employee_name = CharField(null=False)
-	employee_team = CharField(null=False)
+    employee_id = PrimaryKeyField(primary_key=True)
+    employee_name = CharField(null=False)
+    employee_team = CharField(null=False)
     class Meta:
         database = db
