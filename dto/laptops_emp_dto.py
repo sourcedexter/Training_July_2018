9f624bf9e78eb6f0e72be7e2ab2fcd85b2bc6406
ruchi <ruchi@thoughtclan.com>
2018-07-06 16:05:23 +0530

    Add view_laptop_emp details

---
+++ dto/laptops_emp_dto.py
@@ -0,0 +1,12 @@
+class Details:
+    def __init__(self, laptop_id, laptop_ram, laptop_gpu, laptop_os, laptop_company, laptop_storage, employee_name, employee_team):
+        self.laptop_id = laptop_id
+        self.laptop_ram = laptop_ram
+        self.laptop_gpu = laptop_gpu
+        self.laptop_os = laptop_os
+        self.laptop_company = laptop_company
+        self.laptop_storage = laptop_storage
+        self.employee_name = employee_name
+        self.employee_team = employee_team
+
+
--- entities/employee.py
+++ entities/employee.py
@@ -1,13 +1,14 @@
 import os
 from peewee import *
+from peewee import PrimaryKeyField
 from playhouse.db_url import connect
 
 from properties import *
 
-db=_mysql.connect(db_host,db_user,db_password,db_name)
+db= MySQLDatabase(db_name, user = db_user, password = db_password)
 
 class Employee(Model):
-    employee_id = PrimaryKeyField(primary_key=True)
+    employee_id = PrimaryKeyField(primary_key=True)  # type: PrimaryKeyField
     employee_name = CharField(null=False)
     employee_team = CharField(null=False)
     class Meta:
